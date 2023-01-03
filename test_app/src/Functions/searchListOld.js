ea577fb09b86e199d5d9eed603989cc745331153
KataRiegels <62016188+KataRiegels@users.noreply.github.com>
2023-01-03 13:30:49 +0100

    Section 3 bug fix (Fine naming issues fixed)

---
+++ test_app/src/Functions/searchListOld.js
@@ -0,0 +1,93 @@
+import React, { useState, useEffect } from 'react';
+import '../App.css';
+import { 
+   Routes, Route, Link, NavLink, useParams, Outlet
+ } from "react-router-dom";
+
+
+const SearchList = () => {
+
+
+   
+    var href = window.location.href;
+    var searchString_array = href.split('/');
+    var searchcontent_array = href.split('=');
+
+    var searchString = searchString_array[searchString_array.length-1];
+    var searchcontent = searchcontent_array[searchcontent_array.length-1];
+
+    const [names, setNames] = useState([]);
+    const [titles, setTitles] = useState([]);
+    useEffect(() => {
+       fetch('http://localhost:5001/api/search/'+ searchString)
+          .then((response) => response.json())
+          .then((data) => {
+             console.log(data);
+             setNames(data.nameResults.nameResultItems);
+             setTitles(data.titleResults.titleResultItems)
+          })
+          .catch((err) => {
+             console.log(err.message);
+          });
+    }, []);
+
+    useEffect(() => {
+      fetch('http://localhost:5001/api/users/user/searches', 
+          {headers : {'Authorization': 'Basic ' + sessionStorage.getItem('user'), 'Content-Type': 'application/json'} ,
+           method : "POST",
+           body : JSON.stringify({"searchContent" : searchcontent})})
+        .then((response) => response.json())
+        .then((data) => {
+           console.log(data);
+        })
+        .catch((err) => {
+           console.log(err.message);
+        });
+      }, []);
+
+
+    return(
+        <div>
+         
+         <h2>Titles</h2>
+
+         {titles.map((title) => {
+
+            var array = title.basicTitle.url.split('/');
+            var tconst = array[array.length-1];
+
+            return (
+               <>
+                  <div>
+                     <NavLink to={"/titles/" + tconst}>
+                        <div className="list-item">{title.basicTitle.primaryTitle}</div>
+                     </NavLink>
+                  </div>
+               </>
+            );
+            })
+         }
+
+         <h2>Names</h2>
+
+         {names.map((name) => {
+
+            var array = name.basicName.url.split('/');
+            var nconst = array[array.length-1];
+
+            return(
+               <>
+                  <NavLink to={"/names/" + nconst}>
+                           <div className="list-item">{name.basicName.primaryName}</div>
+                  </NavLink>
+               </>
+            )
+
+         })}
+
+      </div>  
+    )
+
+};
+
+export default SearchList;
